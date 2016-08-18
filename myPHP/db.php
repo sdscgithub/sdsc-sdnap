40c7f54d79d0cf342cf4882cd35a231c886613e5
Steven Yeu <yeusteven@gmail.com>
2016-08-17 18:12:48 -0700

    ad db

--- myPHP/db.php
+++ myPHP/db.php
@@ -8,11 +8,11 @@ Description: connect to the sql server
   /* The page that the user will be directed to after adding,
   editing or deleteing */
   //$redirectPage = "Location: https://holonet.sdsc.edu/sdnap/";
-  $redirectPage = "Location: http://localhost/";
+  $redirectPage = "Location: http://localhost:8888";
   /* Holds information on data in the rows */
-  $primaryTable = "test2";
+  $primaryTable = "user_data";
   /* Holds informatin for dropdown menus */
   $secondaryTable = "select_options";
   //$mysqli = new mysqli( "localhost", "sdnapdbuser", '_Apm$wsM4',"sdnapdb");
-  $mysqli = new mysqli( "localhost", "root", '',"test");
+  $mysqli = new mysqli( "localhost:8889", "root", 'root',"test");
  ?>
