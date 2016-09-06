982f6c8f2041898f7db0cd0e017fb55c807935ca
Steven Yeu <yeusteven@gmail.com>
2016-09-06 12:35:41 -0700

    changes to upload file

--- myPHP/uploadFile.php
+++ myPHP/uploadFile.php
@@ -3,6 +3,44 @@
 /* Connect to the database */
 include_once("db.php");
 
+// uploads file to a folder
+$ds = DIRECTORY_SEPARATOR;
+
+$storeFolder = 'files'; // folder for uploaded files
+$path = $_SERVER['DOCUMENT_ROOT'];
+
+if (!empty($_FILES)) {
+
+    $tempFile = $_FILES['file']['tmp_name'];
+
+    $targetPath = $path . $ds. $storeFolder . $ds;
+
+    $targetFile =  $targetPath. $_FILES['file']['name']; // destination of where file will be uploaded to 
+
+    move_uploaded_file($tempFile,$targetFile);
+
+}
+
+// Store filename and file path to sql table to be used a download link.
+
+// $filename = $_FILES['file']['name'];
+// $fileSize = $_FILES['file']['size'];
+//
+// include 'library/config.php';
+// include 'library/opendb.php';
+//
+//
+// $mysqli->query("INSERT INTO `$fileTable` (id) VALUES(DEFAULT)");
+// $id = $mysqli->insert_id;
+//
+//
+// $mysqli->query("INSERT INTO `$fileTable` (name,location) VALUES('$filename','$targetPath')");
+//
+// include 'library/closedb.php';
+
+
+
+
 if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
 {
 $fileName = $_FILES['userfile']['name'];
