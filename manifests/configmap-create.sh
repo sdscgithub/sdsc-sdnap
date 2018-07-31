4b41ba1407e4a60b702b1b09021e36b8c53ea243
Anthony Ha <aha@sdsc.edu>
2018-07-31 12:23:46 -0700

    rearrange site dirs; add tls certs configmap

--- manifests/configmap-create.sh
+++ manifests/configmap-create.sh
@@ -6,8 +6,8 @@ kubectl -n jx delete configmap cert
 kubectl -n jx delete configmap key
 kubectl -n jx delete configmap interm
 
-kubectl -n jx create configmap apache2-config --from-file="sites/etc/httpd/conf/httpd.conf"
-kubectl -n jx create configmap php-config --from-file="sites/etc/php.ini"
-kubectl -n jx create configmap cert --from-file="sites/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
-kubectl -n jx create configmap key --from-file="sites/etc/pki/tls/private/maersk.key"
-kubectl -n jx create configmap cert --from-file="sites/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
+kubectl -n jx create configmap apache2-config --from-file="../site/etc/httpd/conf/httpd.conf"
+kubectl -n jx create configmap php-config --from-file="../site/etc/php.ini"
+kubectl -n jx create configmap cert --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
+kubectl -n jx create configmap key --from-file="../site/etc/pki/tls/private/maersk.key"
+kubectl -n jx create configmap cert --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
