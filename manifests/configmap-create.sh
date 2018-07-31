5a12ede55c35e2a1676d948d9e358c5b5bedb51c
Anthony Ha <aha@sdsc.edu>
2018-07-31 13:18:37 -0700

    change to alpine image for gitlab runner

---
+++ manifests/configmap-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kubectl -n jx delete configmap apache2-config 
+kubectl -n jx delete configmap php-config
+kubectl -n jx delete configmap cert
+kubectl -n jx delete configmap key
+kubectl -n jx delete configmap interm
+
+kubectl -n jx create configmap apache2-config --from-file="./site/etc/httpd/conf/httpd.conf"
+kubectl -n jx create configmap php-config --from-file="./site/etc/php.ini"
+kubectl -n jx create configmap cert --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
+kubectl -n jx create configmap key --from-file="./site/etc/pki/tls/private/maersk.key"
+kubectl -n jx create configmap interm --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
