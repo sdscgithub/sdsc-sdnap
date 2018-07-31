cfa738f15a8f75ffc657124dbfcb2852bb594172
Anthony Ha <aha@sdsc.edu>
2018-07-31 13:02:45 -0700

    cleanup configmap created non-functional manifests for configmaps

--- manifests/configmap-create.sh
+++ manifests/configmap-create.sh
@@ -10,4 +10,4 @@ kubectl -n jx create configmap apache2-config --from-file="../site/etc/httpd/con
 kubectl -n jx create configmap php-config --from-file="../site/etc/php.ini"
 kubectl -n jx create configmap cert --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
 kubectl -n jx create configmap key --from-file="../site/etc/pki/tls/private/maersk.key"
-kubectl -n jx create configmap cert --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
+kubectl -n jx create configmap interm --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
