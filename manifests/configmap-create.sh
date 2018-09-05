a592436e3145be34056cdbcb1483a3631583f02d
Anthony Ha <aha@sdsc.edu>
2018-09-05 12:16:21 -0700

    fix configmap to be specific to site instead of namespace

--- manifests/configmap-create.sh
+++ manifests/configmap-create.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-kubectl -n jx delete configmap apache2-config 
-kubectl -n jx delete configmap php-config
-kubectl -n jx delete configmap cert
-kubectl -n jx delete configmap key
-kubectl -n jx delete configmap interm
+kubectl -n jx delete configmap apache2-config-sdnap 
+kubectl -n jx delete configmap php-config-sdnap
+kubectl -n jx delete configmap cert-sdnap
+kubectl -n jx delete configmap key-sdnap
+kubectl -n jx delete configmap interm-sdnap
 
-kubectl -n jx create configmap apache2-config --from-file="./site/etc/httpd/conf/httpd.conf"
-kubectl -n jx create configmap php-config --from-file="./site/etc/php.ini"
-kubectl -n jx create configmap cert --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
-kubectl -n jx create configmap key --from-file="./site/etc/pki/tls/private/maersk.key"
-kubectl -n jx create configmap interm --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
+kubectl -n jx create configmap apache2-config-sdnap --from-file="../site/etc/httpd/conf/httpd.conf"
+kubectl -n jx create configmap php-config-sdnap --from-file="../site/etc/php.ini"
+kubectl -n jx create configmap cert-sdnap --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
+kubectl -n jx create configmap key-sdnap --from-file="../site/etc/pki/tls/private/maersk.key"
+kubectl -n jx create configmap interm-sdnap --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
