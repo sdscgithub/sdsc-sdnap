d07d742975f189efc011ee0051ee0ff7cfb512cd
Anthony Ha <aha@sdsc.edu>
2018-09-14 09:06:43 -0700

    point db host to service and clean up dir structure

--- site/www/sdnap/myPHP/db.php
+++ site/www/sdnap/myPHP/db.php
@@ -5,13 +5,23 @@ Purpose: connect to the sql server
 Description: connect to the sql server
 */
 
+  $db_host   = $_SERVER["MYSQL_HOST"];
+  $db_user   = $_SERVER["MYSQL_USER"];
+  $db_passwd = $_SERVER["MYSQL_PASSWORD"];
+
   /* The page that the user will be directed to after adding,
   editing or deleting */
   //$redirectPage = "Location: https://holonet.sdsc.edu/sdnap/";
-  $redirectPage = "Location: https://maersk.sdsc.edu/sdnap/";
+  //$redirectPage = "Location: https://maersk.sdsc.edu/sdnap/";
+  $redirectPage = "Location: https://sdnap.sdsc.edu/";
   /* Holds information on data in the rows */
   $primaryTable = "user_data";
   /* Holds informatin for dropdown menus */
   $secondaryTable = "select_options";
-  $mysqli = new mysqli( "localhost", "sdnapdbuser", '_Apm$wsM4',"sdnapdb");
+  /* Connect to db via k8s service in jx namespace */
+  /* $mysqli = new mysqli( "sdnapdb.jx.svc.cluster.local", "mysql", "password","sdnapdb");
+   */
+  $mysqli = new mysqli( $db_host, $db_user, $db_passwd); 
+
  ?>
+
