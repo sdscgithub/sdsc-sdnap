93981648f07fbd5f04cf3de2ef7920d052ad7725
Anthony Ha <aha@sdsc.edu>
2018-09-14 09:20:15 -0700

    correct path for site content

--- manifests/configmap-create.sh
+++ manifests/configmap-create.sh
@@ -6,8 +6,8 @@ kubectl -n jx delete configmap cert-sdnap
 kubectl -n jx delete configmap key-sdnap
 kubectl -n jx delete configmap interm-sdnap
 
-kubectl -n jx create configmap apache2-config-sdnap --from-file="../site/etc/httpd/conf/httpd.conf"
-kubectl -n jx create configmap php-config-sdnap --from-file="../site/etc/php.ini"
-kubectl -n jx create configmap cert-sdnap --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
-kubectl -n jx create configmap key-sdnap --from-file="../site/etc/pki/tls/private/maersk.key"
-kubectl -n jx create configmap interm-sdnap --from-file="../site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
+kubectl -n jx create configmap apache2-config-sdnap --from-file="./site/etc/httpd/conf/httpd.conf"
+kubectl -n jx create configmap php-config-sdnap --from-file="./site/etc/php.ini"
+kubectl -n jx create configmap cert-sdnap --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_cert.crt"
+kubectl -n jx create configmap key-sdnap --from-file="./site/etc/pki/tls/private/maersk.key"
+kubectl -n jx create configmap interm-sdnap --from-file="./site/etc/pki/tls/certs/maersk_sdsc_edu_interm.crt"
